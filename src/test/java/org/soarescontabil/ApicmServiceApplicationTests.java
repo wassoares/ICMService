36c887922117a6995c6b9420f505f23fbd86373f
wassoares <wlfsoares@gmail.com>
2016-08-26 08:40:15 -0300

    first commit

---
+++ src/test/java/org/soarescontabil/ApicmServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.soarescontabil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApicmServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
